1c44dbd1afa8d6eed00a8e8342036a0850efc10f
jairtamara <33210719+jairtamara@users.noreply.github.com>
2024-07-29 16:03:40 -0500

    Update tallerpython.py

--- tallerpython.py
+++ tallerpython.py
@@ -1,25 +1,13 @@
 import git
 
 def get_differences(repo):
-    # Verificar si el repositorio tiene más de un commit
-    if repo.commit_count() > 1:
-        # Obtener el commit actual y el commit anterior con fetch_depth=0
-        repo.git.fetch(depth=0)
-        current_commit = repo.commit('HEAD')
-        previous_commit = repo.commit('HEAD~1')
+    # Obtener las diferencias entre el commit actual y el commit anterior
+    diff = repo.git.diff('HEAD~1', 'HEAD', '--only')
 
-        # Obtener las diferencias entre los dos commits
-        differences = current_commit.diff(previous_commit, create_patch=True)
+    # Crear una lista con las diferencias
+    differences_list = diff.splitlines()
 
-        # Crear una lista con las diferencias
-        differences_list = []
-        for diff in differences:
-            differences_list.append(diff)
-
-        return differences_list
-    else:
-        print("El repositorio solo tiene un commit, no hay diferencias para mostrar")
-        return []
+    return differences_list
 
 # Inicializar el repositorio Git
 repo = git.Repo()
