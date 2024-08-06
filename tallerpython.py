86677bea6e87f9287816e8e01a611428141a83f9
jairtamara <33210719+jairtamara@users.noreply.github.com>
2024-08-06 17:04:06 -0500

    Update tallerpython.py

--- tallerpython.py
+++ tallerpython.py
@@ -2,9 +2,13 @@ import git
 
 def get_differences(repo):
     # Obtener las diferencias entre el commit actual y el commit anterior
-    #diff = repo.git.diff('HEAD~1', 'HEAD', '--name-only')Unix
-  repo = git.Repo('.')
+    repo = git.Repo('.')
+    diff = repo.git.diff('HEAD~1', 'HEAD', '--name-only')Unix
+    for d in diffs:
+        print(f"Archivo modificado: {d.a_path}") 
+  
+
     
-  diffs = repo.index.diff(repo.head.commit)
-  for d in diffs:
-    print(f"Archivo modificado: {d.a_path}") 
+  #diffs = repo.index.diff(repo.head.commit)
+  #for d in diffs:
+    #print(f"Archivo modificado: {d.a_path}") 
