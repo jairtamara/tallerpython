c90b322df8ab394773bbf59330b6ff5c65261b0a
jairtamara <33210719+jairtamara@users.noreply.github.com>
2024-08-06 15:23:41 -0500

    Update tallerpython.py

--- tallerpython.py
+++ tallerpython.py
@@ -8,19 +8,3 @@ def get_differences(repo):
   for d in diffs:
     print(d.a_path)
     print(diffs)
-
-
-    # Crear una lista con las diferencias
-    differences_list = diff.splitlines()
-
-    return differences_list
-
-# Inicializar el repositorio Git
-repo = git.Repo()
-
-# Obtener las diferencias
-differences = get_differences(repo)
-
-# Imprimir las diferencias
-#for difference in differences:
- #   print(difference)
