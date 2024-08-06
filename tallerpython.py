ad8cbaa7ddfcf4a94fb2f7345e2187fc145f07ae
jairtamara <33210719+jairtamara@users.noreply.github.com>
2024-08-06 18:53:28 -0500

    Update tallerpython.py

--- tallerpython.py
+++ tallerpython.py
@@ -6,5 +6,5 @@ def get_differences():
     
     # Obtén las diferencias entre el HEAD y el ORIG_HEAD
     #modified_files = repo.git.diff('HEAD', 'FETCH_HEAD', '--name-only')
-    modified_files = repo.git.diff('--name-only', 'HEAD~1..HEAD')  
+    modified_files = repo.git.diff('--name-only', 'HEAD','HEAD')  
     return modified_files
